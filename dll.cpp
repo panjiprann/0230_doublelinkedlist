20040e8f0b69f2ef244bb1eebffa9d7fb2289573
panjiprann <panjipranantya20@gmail.com>
2024-05-02 15:51:32 +0700

    masukkan node baru kedalam list

--- dll.cpp
+++ dll.cpp
@@ -18,4 +18,24 @@ void addNode() {
     cin >> newNode->noMhs;      //assign value to the data field of the new node
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name;       // assign value to the data field of the new node
+
+
+    // Insert the new node in the list 
+    if (START == NULL || newNode->noMhs <= START->noMhs) {
+
+        if (START != NULL && newNode->noMhs == START->noMhs){
+            cout << "\033[31nDuplicate roll number not allowed\033]0m" << endl;
+            return;
+        }
+
+    }
+}
+
+
+
+
+
+
+int main(){
+
 }

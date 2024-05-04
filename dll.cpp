a07e373c1e4de0240b1c6d8648ebad47cb283763
panjiprann <panjipranantya20@gmail.com>
2024-05-05 00:41:07 +0700

    menambahkan bool emptylist

--- dll.cpp
+++ dll.cpp
@@ -117,10 +117,11 @@ void addNode() {
     // release the memory of the node marked as current
     delete current;
     cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\x1b[0m" << endl;
-
-
-
-}
+  }
+  bool listEmpty()
+  {
+    return (START == NULL);
+  }
     }
     }
 

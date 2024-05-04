5fef3c2e8f177e8568585e81dbfcb613d7d8bbcd
panjiprann <panjipranantya20@gmail.com>
2024-05-05 00:12:13 +0700

    membuat step 1.b

--- dll.cpp
+++ dll.cpp
@@ -34,7 +34,8 @@ void addNode() {
         }
         else{
         // insert the new node in the middle or at the end
-        Node *current = START; // step 1.a : start from the first node        
+        Node *current = START; // step 1.a : start from the first node
+        Node *previous = NULL; // step 1.b : previous node is NULL initially       
     }
 }
 

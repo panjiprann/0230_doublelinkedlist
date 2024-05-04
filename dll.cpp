29c33e857a55e208506ecb5a9cd1ceb71f79dce0
panjiprann <panjipranantya20@gmail.com>
2024-05-05 00:44:19 +0700

    step 1,2,3

--- dll.cpp
+++ dll.cpp
@@ -121,7 +121,22 @@ void addNode() {
   bool listEmpty()
   {
     return (START == NULL);
-  }
+}
+void traverse()
+{
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecords in ascending order of roll number are : " << endl;
+        Node *currentNode = START; // step 1
+        while (currentNode != NULL) // step 2
+        {
+             cout << currentNode->noMhs << " " << currentNode->name << endl; // step 3
+        }
+    }
+
+}
     }
     }
 

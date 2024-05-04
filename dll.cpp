23c832e80d038fbbde3d27f38c6473e6a54feae1
panjiprann <panjipranantya20@gmail.com>
2024-05-05 00:46:36 +0700

    membuat void revtravese

--- dll.cpp
+++ dll.cpp
@@ -135,8 +135,26 @@ void traverse()
              cout << currentNode->noMhs << " " << currentNode->name << endl; // step 3
         }
     }
-
 }
+
+void revtravese()
+{
+    if (listEmpty())
+    cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecords in descending order of roll number are : " << endl;
+        Node *currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+
+    }
     }
     }
 
